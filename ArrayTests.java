26f4016e7b3afc78f330f1157156b21eff6db38b
vihsiao <vihsiao@ucsd.edu>
2022-10-12 17:06:38 -0700

    fixed reversed() and reverseInPlace() methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1,2,3,4,5,6,7,8,9,10,11};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{11,10,9,8,7,6,5,4,3,2,1}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {1,0,10,15};
+    assertArrayEquals(new int[]{15, 10, 0, 1}, ArrayExamples.reversed(input1));
   }
 }
