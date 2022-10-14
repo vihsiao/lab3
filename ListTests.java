756ad708ec783cbcb4e1c8065f5a86413a7b6bf6
vihsiao <vihsiao@ucsd.edu>
2022-10-14 10:09:13 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+public class ListTests {
+    @Test
+    public void testMerge() {
+        List<String> list1 = new ArrayList<String>();
+        List<String> list2 = new ArrayList<String>();
+    }
+}
